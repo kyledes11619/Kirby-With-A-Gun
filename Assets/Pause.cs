7764caf0163cce9683c578bba55f89df3b9b9d7e
Celuan1999 <ch520385@ucf.edu>
2024-02-11 17:57:52 -0500

    All main menus are working

--- Assets/Pause.cs
+++ Assets/Pause.cs
@@ -11,7 +11,7 @@ public class Pause : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (isPaused)
             {
